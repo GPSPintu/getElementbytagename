de1858a26b53bb91d79c11c9538ddff8402a36d7
PINTU PANDIT <122666688+GPSPintu@users.noreply.github.com>
2023-02-14 19:35:11 +0530

    Add files via upload

---
+++ tagclass.js
@@ -0,0 +1,17 @@
+//GETELEMENTSBYTAGNAME//
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'hello 2';
+li[1].style.fontWeight = 'bold'
+li[1].style.backgroundColor = 'yellow';
+ 
+
+///Gives error//
+// items.style.backgroundColor = '#f4f4f4';
+// for(var i = 0 ; i < li.length; i++){
+//     li[i].style.backgroundColor = '#f4f4f4';
+// }
+
+
+
